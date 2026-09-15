16ee90e4209562eee2aca122fe25d599165c1192
Vova Muzychko <volodymyr.muzychko@stu.ocsb.ca>
2026-09-15 14:57:12 +0000

    First commit of helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2026 VovaM  All rights reserved.
+// .
+// Created by: VovaM
+// Date: Sep 15th, 2026
+// This program displays a greeting with proper style.
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Ms Raffin" << std::endl;
+    std::cout << "I LOVE Programming" << std::endl;
+    std::cout <<"This is so much fun" << std::endl;
+}
